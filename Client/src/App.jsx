6e5f0f7cf3bd9f2651d103f9a8c29cf082dec06c
Harshal <harshal0921@gmail.com>
2024-02-04 11:28:41 +0530

    create pages and define routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import createBooks from './pages/createBooks.jsx'
+import deleteBook from './pages/deleteBook.jsx'
+import editBook from './pages/editBook.jsx'
+import showBooks from './pages/showBooks.jsx'
+import Home from './pages/Home.jsx'
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
+    <Routes>
+      <Route path='' element={Home} />
+      <Route path='' element={showBooks} />
+      <Route path='' element={createBooks} />
+      <Route path='' element={editBook} />
+      <Route path='' element={deleteBook} />
+    </Routes>
   )
 }
 
