938856203fcb502b1219a803b3db243e288ae4af
Harshal <harshal0921@gmail.com>
2024-07-03 22:05:30 +0530

    update editBook.jsx

--- Client/src/pages/editBook.jsx
+++ Client/src/pages/editBook.jsx
@@ -38,7 +38,7 @@ const EditBook = () => {
     };
     setLoading(true);
     axios
-      .put(`http://localhost:5555/books/${id}`, data)
+      .put(`${SERVER_URL}/books/${id}`, data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Edited successfully', { variant: 'success' });
